1398eebd209b7aaedee9511bac56a6682347be53
KovalSergej <koval.sergej.2014@gmail.com>
2020-09-27 19:48:26 +0300

    05 first commit

--- task-03.js
+++ task-03.js
@@ -1,2 +1,33 @@
 "use strict";
 
+class Storage {
+  constructor(dataArray) {
+    this.items = dataArray;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    this.items.push(item);
+  }
+  removeItem(item) {
+    const delItem = this.items.indexOf(item);
+    this.items.splice(delItem, 1);
+  }
+}
+
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор",
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem("Дроид");
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem("Пролонгер");
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
--- task-04.js
+++ task-04.js
@@ -1,1 +1,37 @@
 "use strict";
+
+class StringBuilder {
+  constructor(val) {
+    this._value = val;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  append(val) {
+    this._value = this._value + val;
+    return this._value;
+  }
+
+  prepend(val) {
+    this._value = val + this._value;
+    return this._value;
+  }
+
+  pad(val) {
+    this._value = val + this._value + val;
+    return this._value;
+  }
+}
+
+const builder = new StringBuilder(".");
+
+builder.append("^");
+console.log(builder.value); // '.^'
+
+builder.prepend("^");
+console.log(builder.value); // '^.^'
+
+builder.pad("=");
+console.log(builder.value); // '=^.^='
